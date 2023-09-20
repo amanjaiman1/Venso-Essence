85a596b9f62fa98390a0aa2468d8943265190606
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-09-20 23:03:38 +0530

    add: label shadcnui added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,24 @@
-export default function Home() {
+"use client";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+
+const SetupPage = ()=> {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if(!isOpen) {
+      onOpen();
+    }
+  },  [isOpen, onOpen]);
     return (
-      <p>Admin Panel Dashboard</p>
+      <div className="p-4">
+        Root Page
+      </div>
     )
   }
+
+export default SetupPage
