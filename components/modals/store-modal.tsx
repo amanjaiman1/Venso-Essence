b86c9710a229cffdf7a3451b7f0ee561fc0f83a4
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-09-20 22:49:41 +0530

    add:  Modals buildup

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import * as z from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "@/components/ui/modal";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const formSchema = z.object({
+    name: z.string().min(1),
+});
+
+export const StoreModal=() => {
+    const storeModal = useStoreModal();
+
+    const form =useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>)  => {
+        console.log(values);
+    }
+
+    return (
+        <Modal
+            title="Create Store"
+            description= "Add a new store to manage products and categories"
+            isOpen= {storeModal.isOpen}
+            onClose={storeModal.onClose}
+        >
+            <div>
+                <div  className="space-y-4 py-2 pb-4">
+                    <Form {...form}>
+                        <form  onSubmit={form.handleSubmit(onSubmit)}> 
+                            <FormField
+                                control = {form.control}
+                                name = "name"
+                                render = {({field}) =>(
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="E-commerce" {...field} />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <div className="pt-6 space-x-2 flex items-center  justify-end">
+                                <Button>Cancel</Button>
+                                <Button>Continue</Button>
+                            </div>
+                        </form>
+                    </Form>
+                </div>
+            </div>
+
+        </Modal>
+    );
+};
